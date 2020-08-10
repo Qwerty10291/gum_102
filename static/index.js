8439dde60aa238c17db518d449ff87bbbc1a143f
Andrey <andrtazet@gmail.com>
2020-08-10 15:44:11 +0300

    0.0.2

--- static/index.js
+++ static/index.js
@@ -4,3 +4,19 @@ let message_container = $('.chat_container')
 let title = $('title');
 let chat = [];
 
+function send_message(){
+    $.post('/add_message', {'text':message_input.val()});
+}
+
+function add_message(login, text){
+    $()
+}
+
+function get_last_message() {
+    $.get('/load_message').done(function (data){
+        let text = JSON.parse(data);
+        chat.push(text);
+    })
+}
+
+message_button.click(add_message);
